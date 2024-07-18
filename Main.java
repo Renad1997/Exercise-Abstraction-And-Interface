f44a8c8611758f673ac6a18cd01666828df12df0
Renad Almana <52034735+Renad1997@users.noreply.github.com>
2024-07-18 06:10:18 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /*Q1*/
+//    Book book1 = new Book("my book1",20.0,"renad");
+//    System.out.println(book1.getDiscount());
+//
+//    Movie movie1 = new Movie("my movie1",50.0,"mohammed");
+//    System.out.println(movie1.getDiscount());
+
+
+        /*Q2*/
+
+//MovablePoint movablePoint1 = new MovablePoint(1,2,3,4);
+//
+//
+//movablePoint1.moveUp();
+//movablePoint1.moveDown();
+//movablePoint1.moveLeft();
+//movablePoint1.moveRight();
+
+
+    }
+}
